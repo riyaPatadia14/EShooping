3b491b69680fb5901e8bbff289fa9aceee79b2d9
Riya Patadia <riyapatadia14@gmail.com>
2023-12-22 18:30:37 +0530

    cart functionality

---
+++ DataAccessLayer/Implementations/CartRepository.cs
@@ -0,0 +1,22 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.GenericRepo;
+using DataAccessLayer.Interface;
+using DataAccessLayer.Models.OrderDetailsSet;
+
+namespace DataAccessLayer.Implementations
+{
+    public class CartRepository : ICart
+    {
+        private readonly IGenericRepository<OrderDetailsModel> _genericRepository;
+        private readonly EShoppingDbContext _eShoppingDbContext;
+        public CartRepository(EShoppingDbContext eShoppingDbContext)
+        {
+            _eShoppingDbContext= eShoppingDbContext;
+        }
+        private bool HasCart(string productId)
+        {
+            var result = _eShoppingDbContext.OrderDetails.FirstOrDefault();
+            return result == null;
+        }
+    }
+}
